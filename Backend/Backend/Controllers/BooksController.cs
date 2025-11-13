15f133253ccf824ed3abb38d48cb694972114b76
Liav Zilbershtein <liav.zilbershtein@synerion.com>
2025-11-13 17:32:54 +0200

    initial backend code. added method name to open api document

---
+++ Backend/Backend/Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿namespace Backend.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private static readonly List<Book> Books = new()
+        {
+            new Book { Title = "The Hobbit", Author = "J.R.R. Tolkien" },
+            new Book { Title = "1984", Author = "George Orwell" },
+            new Book { Title = "To Kill a Mockingbird", Author = "Harper Lee" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return Books;
+        }
+
+        [HttpPost]
+        public Book AddBook([FromBody] Book book)
+        {
+            Books.Add(book);
+            return book;
+        }
+    }
+
+    public class Book
+    {
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+    }
+}
---
+++ Backend/Backend/OpenApi/AddMethodNameOperationTransformer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace Backend.OpenApi;
+
+public class AddMethodNameOperationTransformer : IOpenApiOperationTransformer
+{
+    public async Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context,
+        CancellationToken cancellationToken)
+    {
+        if (context.Description.ActionDescriptor is not ControllerActionDescriptor controllerActionDescriptor)
+            return;
+        operation.AddExtension("x-method-name", new JsonNodeExtension(controllerActionDescriptor.ActionName));
+    }
+}
